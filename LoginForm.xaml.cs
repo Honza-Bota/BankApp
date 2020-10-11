de8a71f87d8183856144ebb258958ace082ea50c
Boťa Jan <botajan@email.cz>
2020-10-12 00:04:04 +0200

    Základní návrh formůláře Login a reakce na tlačítka.

--- LoginForm.xaml.cs
+++ LoginForm.xaml.cs
@@ -23,5 +23,37 @@ namespace BankApp
         {
             InitializeComponent();
         }
+
+        private void butOk_Click(object sender, RoutedEventArgs e)
+        {
+            string name = tbName.Text;
+            string password = tbPassword.Password;
+
+            if (name == "admin" && password == "root")
+            {
+                BankInterfaceForm bank = new BankInterfaceForm();
+                bank.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Špatné přihlašovací údaje",
+                            "Chyba v ověření",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Warning,
+                            MessageBoxResult.OK);
+            }
+        }
+
+        private void butCancel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult rslt = MessageBox.Show("Chcete opravdu ukončit aplikaci?",
+                                                    "Ukončování",
+                                                    MessageBoxButton.YesNo,
+                                                    MessageBoxImage.Question,
+                                                    MessageBoxResult.No);
+
+            if (rslt == MessageBoxResult.Yes) this.Close();
+        }
     }
 }
