b20fb49185f1fea53571cd4d6f6d5a8f66ceeac5
Boťa Jan <botajan@email.cz>
2020-11-03 01:26:58 +0100

    Studentský účet - specializované vklady a výběry

--- StudentAccount.cs
+++ StudentAccount.cs
@@ -8,8 +8,31 @@ namespace BankApp
 {
     class StudentAccount : DebetAccount
     {
-        public StudentAccount(long accountNumber, string name, string surname, DateTime birthdate, AccountTypes accountType, int moneyValue = 0) : base(accountNumber, name, surname, birthdate, accountType, moneyValue)
+        public StudentAccount(long accountNumber,
+                              string name,
+                              string surname,
+                              DateTime birthdate,
+                              AccountTypes accountType,
+                              int moneyValue = 0) : base(accountNumber, name, surname, birthdate, accountType, moneyValue)
         {
+        }
+
+        public override bool Witherdraw(int amount, string message)
+        {
+            bool witherdrawed = false;
+
+            if (amount > 0 && amount <= Settings.StudentLimitWitherdraw && amount < MoneyValue)
+            {
+                MoneyValue -= amount;
+                LogTransaction($"Vybrána částka {amount} se zprávou {message}");
+                witherdrawed = true;
+            }
+            else
+            {
+                witherdrawed = false;
+            }
+
+            return witherdrawed;
         }
     }
 }
